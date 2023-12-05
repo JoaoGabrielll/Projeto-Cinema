b35f7bbe5129cd4334eea138090e94599f02ab3f
Joao <joaogabrielsb36@gmail.com>
2023-12-04 22:49:23 -0300

    Construção do metodo de venda dos ingressos

--- src/entities/Cinema.java
+++ src/entities/Cinema.java
@@ -1,6 +1,43 @@
 package entities;
 
 public class Cinema {
-    private int fileiras;
-    private int assentos;
+    private int numeroFileiras;
+    private int cadeirasEmCadaFileira;
+    private double valorIngresso;
+
+    public Cinema(int numeroFileiras, int assentosEmCadaFileira) {
+        this.numeroFileiras = numeroFileiras;
+        this.cadeirasEmCadaFileira = assentosEmCadaFileira;
+    }
+
+    public int getNumeroFileiras() {
+        return numeroFileiras;
+    }
+
+    public void setNumeroFileiras(int numeroFileiras) {
+        this.numeroFileiras = numeroFileiras;
+    }
+
+    public int getCadeirasEmCadaFileira() {
+        return cadeirasEmCadaFileira;
+    }
+
+    public void setCadeirasEmCadaFileira(int assentosEmCadaFileira) {
+        this.cadeirasEmCadaFileira = assentosEmCadaFileira;
+    }
+
+    public double getValorIngresso() {
+        return valorIngresso;
+    }
+
+    public void setValorIngresso(double valorIngresso) {
+        this.valorIngresso = valorIngresso;
+    }
+
+    public String cadeirasCinema() {
+        if (this.cadeirasEmCadaFileira * this.numeroFileiras < 60) {
+            this.valorIngresso = (this.cadeirasEmCadaFileira * this.numeroFileiras) * 10.0;
+        }
+        return "Resultado Total: R$" + this.valorIngresso;
+    }
 }
--- src/program/MainClass.java
+++ src/program/MainClass.java
@@ -1,12 +1,23 @@
 package program;
 
+import entities.Cinema;
+
 import java.util.Scanner;
 
 public class MainClass {
     public static void main(String[]args){
         Scanner tc = new Scanner(System.in);
 
+        int numeroFileiras, cadeirasEmCadaFileira;
 
+        System.out.print("Numeros de Fileiras: ");
+        numeroFileiras = tc.nextInt();
+        System.out.print("Numero de cadeiras por fila: ");
+        cadeirasEmCadaFileira = tc.nextInt();
+
+        Cinema cinema = new Cinema(numeroFileiras, cadeirasEmCadaFileira);
+
+        System.out.println(cinema.cadeirasCinema());
         tc.close();
     }
 }
